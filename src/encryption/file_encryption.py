5774a94bb933aa65a2787c33388183f96d883b00
Jon Fox <jon@foxsolutions.dev>
2024-04-07 15:44:06 -0500

    key generator separated

--- src/encryption/file_encryption.py
+++ src/encryption/file_encryption.py
@@ -1,14 +1,7 @@
 # import required module
 from cryptography.fernet import Fernet
 
-# key generation
-key = Fernet.generate_key()
- 
-# string the key in a file
-with open('filekey.key', 'wb') as filekey:
-   filekey.write(key)
-
-# opening the key
+# reading generated key
 with open('filekey.key', 'rb') as filekey:
 	key = filekey.read()
 
